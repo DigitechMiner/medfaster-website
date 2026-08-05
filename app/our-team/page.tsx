86fbe2f465481b8d31487a90f3f2e94c083a4774
Digitech Miner <developer@digitechminer.in>
2026-08-06 03:43:29 +0530

    team styling updated

--- app/our-team/page.tsx
+++ app/our-team/page.tsx
@@ -240,7 +240,7 @@ export default function TeamPage() {
               name={member.name}
               role={member.role}
               //bio={member.bio}
-              image={member.image}
+              image={member.image ?? "/images/team/default-profile.png"}
               social={member.social}
             />
           ))}
